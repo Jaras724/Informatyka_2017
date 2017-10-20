039d2471885d7ab10798fad661090f7e410ba4ec
Jaras724 <32752258+Jaras724@users.noreply.github.com>
2017-10-20 08:50:05 +0200

    Add files via upload

---
+++ 1_5 (rozwiązanie równania kwadratowego, delta).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double
+    a, b, c, delta, x, x1, x2;
+    cin>>a>>b>>c;
+    if (a==0) {cout<< "Bardzo mi przykro, ale to nie jest rownanie kwadratowe :("<<endl;
+    } else {
+        delta=b*b-4*a*c;
+
+
+        if (delta<0) { cout<<"Bardzo mi przykro, ale to rownanie nie ma pierwiastkow :("<<endl;
+    } else {
+
+        if (delta==0) {
+        (x=-b/(2*a));
+    cout<<"X wynosi: " <<x<< endl; }
+    else {
+        (x1=(-b-sqrt(delta))/(2*a));
+        (x2=(-b+sqrt(delta))/(2*a));
+        cout <<"x1 wynosi: "<<x1<<endl;
+        cout <<"x2 wynosi: "<<x2<<endl;
+}
+}
+    }
+}
