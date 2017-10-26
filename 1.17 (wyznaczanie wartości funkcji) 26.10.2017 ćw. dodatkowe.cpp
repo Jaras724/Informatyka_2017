716e64c1a0980555e074d4c99c0d5b65d6776675
Jaras724 <32752258+Jaras724@users.noreply.github.com>
2017-10-26 19:27:05 +0200

    Add files via upload

---
+++ 1.17 (wyznaczanie wartości funkcji) 26.10.2017 ćw. dodatkowe.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+
+{
+{
+double
+x;
+    cout<<"Podaj x"<<endl;
+    cin>>x;
+
+
+    if (x<7)
+        cout<<((x*x*x)+1)<<endl;
+        else
+            if (x==7)
+            cout<<(cos(x-1))<<endl;
+        else
+            if (x==9)
+            cout<<(sqrt(3*x))<<endl;
+        else
+            cout<<(-8*x)<<endl;
+}
+return 0;
+}
